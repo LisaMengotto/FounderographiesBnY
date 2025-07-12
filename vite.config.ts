5fc00563af3cd9bdfe9dd7b6a27b168fd7310a48
Lisa Mengotto <78284071+LisaMengotto@users.noreply.github.com>
2025-07-12 12:13:58 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/your-repo-name/', // 👈 IMPORTANT: replace with your actual repo name
+  base: '/FounderographiesBnY/', 
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
